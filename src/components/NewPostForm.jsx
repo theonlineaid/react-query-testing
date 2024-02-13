b288bc6eda764f35527f516128040375de7afa9d
theonlineaid <nkomorfaruk@gmail.com>
2024-02-13 17:44:11 +0600

    Add test data

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,49 +1,32 @@
 import React, { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
-// import { addNewPost } from '../fetch';
+import { addNewPost } from '../fetch';
 
 
 export default function NewPostForm() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
-    const {mutation, isLoading} = useMutation({
-        mutationFn: async (formData) => {
-            const res = await fetch('https://dummyjson.com/products/add', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            return data.products;
-        },
-        onError: (error) => {
-            console.error('Error adding new post:', error);
-        },
-        onSuccess: () => {
-            // Clear input fields after successful submission
-            setTitle('');
-            setDescription('');
-        },
+    const mutation = useMutation({
+        mutationFn: addNewPost
     });
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        mutation.mutate({ title })
         // Check if title and description are not empty
-        if (title.trim() !== '' && description.trim() !== '') {
-            try {
-                await  mutation.mutate({ title, description });
-                // Clear input fields after successful submission
-                setTitle('');
-                setDescription('');
-            } catch (error) {
-                console.error('Error adding new post:', error);
-            }
-        } else {
-            alert('Please enter title and description.');
-        }
+        // if (title.trim() !== '' && description.trim() !== '') {
+        //     try {
+        //         await  mutation.mutate({ title, description });
+        //         // Clear input fields after successful submission
+        //         setTitle('');
+        //         setDescription('');
+        //     } catch (error) {
+        //         console.error('Error adding new post:', error);
+        //     }
+        // } else {
+        //     alert('Please enter title and description.');
+        // }
     };
 
     return (
@@ -54,14 +37,16 @@ export default function NewPostForm() {
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
             />
-            <textarea
+            {/* <textarea
                 placeholder='Enter content'
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
-            />
-            <button type='submit' disabled={isLoading}>
+            /> */}
+            {/* <button type='submit' disabled={isLoading}>
                 {isLoading ? 'Adding...' : 'Add Post'}
-            </button>
+            </button> */}
+
+            <button type='submit'>Add</button>
         </form>
     );
 }
